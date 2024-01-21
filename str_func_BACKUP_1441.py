41d2b3356b945169c1c8e4e759fca8a69b9db0f3
Maksim Bodnar <maximsevsk@gmail.com>
2024-01-21 22:50:53 +0300

    add docstring and to_titlecase function

---
+++ str_func_BACKUP_1441.py
@@ -0,0 +1,12 @@
+def to_uppercase(input_string):
+<<<<<<< HEAD
+    """Возвращает строку заглавными буквами"""
+=======
+    """я вредный и все испорчу"""
+>>>>>>> develop
+    return input_string.upper()
+
+
+def to_titlecase(input_string):
+    """Делает заглавными первые буквы каждого слва"""
+    return input_string.title()
---
+++ str_func_LOCAL_1441.py
@@ -0,0 +1,8 @@
+def to_uppercase(input_string):
+    """Возвращает строку заглавными буквами"""
+    return input_string.upper()
+
+
+def to_titlecase(input_string):
+    """Делает заглавными первые буквы каждого слва"""
+    return input_string.title()
